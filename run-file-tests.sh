6c1666bc9b05b54702d2b1851cab7c7c71b5bca9
Jan Kaluza <jkaluza@redhat.com>
2014-04-07 13:32:45 +0200

    try calling file directly

--- run-file-tests.sh
+++ run-file-tests.sh
@@ -3,6 +3,9 @@
 cp ./src/.libs/* ./file-tests
 cd file-tests
 LD_LIBRARY_PATH=. python ./fast-regression-test.py "./file -m ../magic/magic.mgc" exact
+echo "EXECUTING:"
+LD_LIBRARY_PATH=. ./file -m ../magic/magic.mgc -b db/wav/send.wav
+LD_LIBRARY_PATH=. ./file -m ../magic/magic.mgc -bi db/wav/send.wav
 ret=$?
 cd ..
 
