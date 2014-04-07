7bde05b6bfa1ccadad21ad09bd372aa0fa95a78e
Jan Kaluza <jkaluza@redhat.com>
2014-04-07 13:11:55 +0200

    Fix run-file-tests.sh

--- run-file-tests.sh
+++ run-file-tests.sh
@@ -2,7 +2,7 @@
 
 cp ./src/.libs/* ./file-tests
 cd file-tests
-LD_LIBRARY_PATH=. python ./fast-regression-test.py ./file exact
+LD_LIBRARY_PATH=. python ./fast-regression-test.py "./file -m ../magic/magic.mgc" exact
 ret=$?
 cd ..
 
