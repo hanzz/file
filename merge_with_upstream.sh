716d7a3b07daf8ff140f965d8badf447142816a4
Jan Kaluza <jkaluza@redhat.com>
2014-05-15 08:00:09 +0200

    merge with upstream

--- merge_with_upstream.sh
+++ merge_with_upstream.sh
@@ -3,5 +3,6 @@ git checkout master
 git merge -s ours upstream/master
 git diff -R upstream/master src/ magic/ > x.patch
 patch -p1 < x.patch
+git add ./magic/Magdir/*
 git commit -a -m 'merge with upstream'
 git push
