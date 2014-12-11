7dbbe83420b0bdec915b47384e88c0ef31682aee
Jan Kaluza - file-tests <jkaluza@redhat.com>
2014-12-11 03:07:22 -0500

    merge with upstream

--- merge_with_upstream.sh
+++ merge_with_upstream.sh
@@ -1,6 +1,6 @@
 git fetch upstream
 git checkout master
-git merge -s ours upstream/master
+git merge -s ours upstream/master -m "merge with upstream"
 git diff -R upstream/master src/ magic/ > x.patch
 patch -p1 < x.patch
 git add ./magic/Magdir/*
